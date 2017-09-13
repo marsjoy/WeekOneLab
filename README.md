9667f076f0eabecfbf7a407e41d9166da9a5a1af
Mars Willilams <mars.williams@airbnb.com>
2017-09-12 17:59:47 -0700

    typeface styling added

---
+++ README.md
@@ -0,0 +1,6 @@
+# WeekOneLab
+CodePath Week One Lab
+* https://courses.codepath.com/courses/intro_to_android/unit/1#!exercises
+
+To Do
+* Use CustomTypefaceSpan (See: https://stackoverflow.com/questions/10675070/multiple-typeface-in-single-textview)
--- app/src/main/java/mars_williams/week_one_lab/LoginActivity.java
+++ app/src/main/java/mars_williams/week_one_lab/LoginActivity.java
@@ -7,52 +7,81 @@ import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
 
+    private Typeface franklinLight;
+    private Typeface franklinRegular;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        typefaceLight();
+        typefaceRegular();
         setUsernameAndPassword();
         setLogIn();
         setOr();
         setFacebookLogIn();
+        setForgotLoginDetails();
         setHelpSigningIn();
+        setNoAccount();
         setSignUp();
     }
 
-    private void setFont(TextView textView) {
-        Typeface font = Typeface.createFromAsset(getAssets(), "fonts/LibreFranklin-Light.ttf");
-        textView.setTypeface(font);
+    private void typefaceLight() {
+        franklinLight = Typeface.createFromAsset(getAssets(), "fonts/LibreFranklin-Light.ttf");
+    }
+
+    private void typefaceRegular() {
+        franklinRegular = Typeface.createFromAsset(getAssets(), "fonts/LibreFranklin-Regular.ttf");
+    }
+
+    private void setTypefaceLight(TextView textView) {
+        textView.setTypeface(franklinLight);
+    }
+
+    private void setTypefaceRegular(TextView textView) {
+        textView.setTypeface(franklinRegular);
     }
 
     private void setUsernameAndPassword() {
         TextView username = (TextView) findViewById(R.id.username);
         TextView password = (TextView) findViewById(R.id.password);
-        setFont(username);
-        setFont(password);
+        setTypefaceLight(username);
+        setTypefaceLight(password);
     }
 
     private void setLogIn() {
         TextView logIn = (TextView) findViewById(R.id.buttonLogin);
-        setFont(logIn);
+        setTypefaceRegular(logIn);
+    }
+
+    private void setForgotLoginDetails() {
+        TextView forgotLoginDetails = (TextView) findViewById(R.id.forgotLoginDetails);
+        setTypefaceLight(forgotLoginDetails);
     }
 
     private void setHelpSigningIn() {
         TextView helpSigningIn = (TextView) findViewById(R.id.helpSigningIn);
-        setFont(helpSigningIn);
+        setTypefaceRegular(helpSigningIn);
     }
+
 
     private void setOr() {
         TextView or = (TextView) findViewById(R.id.or);
-        setFont(or);
+        setTypefaceLight(or);
     }
 
     private void setFacebookLogIn() {
         TextView facebookLogIn = (TextView) findViewById(R.id.facebookLogIn);
-        setFont(facebookLogIn);
+        setTypefaceRegular(facebookLogIn);
+    }
+
+    private void setNoAccount() {
+        TextView noAccount = (TextView) findViewById(R.id.noAccount);
+        setTypefaceLight(noAccount);
     }
 
     private void setSignUp() {
         TextView signUp = (TextView) findViewById(R.id.signUp);
-        setFont(signUp);
+        setTypefaceRegular(signUp);
     }
 }
